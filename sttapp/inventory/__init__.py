b26e129564a0e7f48f7393fdaf7b681c7b07c097
Christian Gerlach <christianrgerlach@gmail.com>
2021-10-21 21:57:31 -0700

    moar

--- sttapp/inventory/__init__.py
+++ sttapp/inventory/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def run_inventory(basepath):
 	paths = []
-	print("inventorying base path:" + basepath)
+	print("Inventorying base path: " + basepath)
 	for path in Path(basepath).rglob('*.wav'):
 	    path = path.relative_to(basepath)
 	    paths.append(path)
