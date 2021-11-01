eba198203d2e2a168363914c5e38a82edbf32fc9
rzahrae <44071528+rzahrae@users.noreply.github.com>
2021-11-01 11:04:10 -0700

    Revert "Christian's weekend changes"
    This reverts commit 5f73cd2213440ef17c266752ab508ff13c4000a8.

--- sttapp/static/script.js
+++ sttapp/static/script.js
@@ -6,25 +6,12 @@ $(document).ready(function() {
         //console.error(error)
     }
 
-    $(".playback_rate").change(function(event) {
-        $(".playback_rate").val(this.value)
-        var players = $(".audio_player")
+    $('.playback_rate').change(function(event) {
+        $('.playback_rate').val(this.value)
+        var players = $('.audio_player')
 
         for (let i = 0; i < players.length; i++) {
             players[i].playbackRate = this.value
         }
-
-        if (this.value != 1) { $(".playback_rate_label").text("Rate=" + this.value) }
-    });
-
-    $(".playback_rate_reset").click(function(event) {
-        $(".playback_rate").val(1)
-        var players = $(".audio_player")
-
-        for (let i = 0; i < players.length; i++) {
-            players[i].playbackRate = 1
-        }
-
-        $(".playback_rate_label").text("Rate")
     });
 });
